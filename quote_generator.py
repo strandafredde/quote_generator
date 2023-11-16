baaa7188f2e6c9fe90cebde9be03c391c38b72b7
strandafredde <strandafredde@gmail.com>
2023-11-16 22:12:27 +0100

    Working version 2.0
    The text is now fitting inside the image

--- quote_generator.py
+++ quote_generator.py
@@ -13,6 +13,29 @@ template_folder = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__, template_folder=template_folder)
 
 
+def draw_text_wrapped(draw, text, font, max_width):
+    words = text.split()
+    lines = []
+    current_line = words[0]
+
+    for word in words[1:]:
+        # Check the width of the line with the new word
+        test_line = current_line + ' ' + word
+        bbox = draw.textbbox((0, 0), test_line, font=font)
+
+        # If the line exceeds the maximum width, start a new line
+        if bbox[2] <= max_width:
+            current_line = test_line
+        else:
+            lines.append(current_line)
+            current_line = word
+
+    # Add the last line
+    lines.append(current_line)
+
+    return lines
+
+
 def img_with_quote():
     quote = pow.quote_random(2, False)
     api_url = 'https://picsum.photos/800/600'
@@ -24,16 +47,28 @@ def img_with_quote():
     # Create a drawing context
     draw = ImageDraw.Draw(image)
 
-    # Use a default font and size
-    font = ImageFont.truetype("arial.ttf", 15)
+    # Use a default font size
+    font_size = 35
+
+    # Load a default font with the initial font size
+    font = ImageFont.truetype("arial.ttf", font_size)
+
+    # Define a maximum width for the text (adjust as needed)
+    max_text_width = 0.8 * image.width
+
+    # Get the wrapped lines of text
+    lines = draw_text_wrapped(draw, quote, font, max_text_width)
 
     # Calculate text position
-    text_bbox = draw.textbbox((0, 0), quote, font=font)
-    text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
-    text_position = ((image.width - text_width) // 2, (image.height - text_height) // 2)
+    text_height = sum(draw.textbbox((0, 0), line, font=font)[3] - draw.textbbox((0, 0), line, font=font)[1] for line in lines)
+    text_position = ((image.width - max_text_width) // 2, (image.height - text_height) // 2)
 
-    # Add the quote to the image
-    draw.text(text_position, quote, font=font, fill=(255, 255, 255))
+    # Draw each line of text
+    for line in lines:
+        bbox = draw.textbbox((0, 0), line, font=font)
+        line_height = bbox[3] - bbox[1]
+        draw.text((text_position[0], text_position[1]), line, font=font, fill=(255, 255, 255), stroke_width=1, stroke_fill=(0, 0, 0))
+        text_position = (text_position[0], text_position[1] + line_height)
 
     # Save the modified image back to BytesIO
     modified_image_data = BytesIO()
